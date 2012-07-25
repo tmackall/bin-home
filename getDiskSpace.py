6fbc078404f0956673cdbdde744c769713415180
Tom Mackall <mackall.tom@gmail.com>
2012-07-25 05:08:58 -0600

    added logging and cleaned up some messages

--- getDiskSpace.py
+++ getDiskSpace.py
@@ -1,26 +1,47 @@
 #!/usr/bin/env python2.7
-import subprocess
-import re
-from sys import exit
-import pexpect
+import logging
 from libEmailTools import emailMessage
 from optparse import OptionParser
+import pexpect
+import re
+import subprocess
+from sys import exit
+
+LOGGING_LEVELS = {'critical': logging.CRITICAL,
+                  'error': logging.ERROR,
+                  'warning': logging.WARNING,
+                  'info': logging.INFO,
+                  'debug': logging.DEBUG}
 
 def Main():
 # command-line parsing
     parser = OptionParser()
-    parser.add_option("--threshold",default=45)
-    parser.add_option("--personToWarn",default='tmackall@qualcomm.com')
+    parser.add_option('--threshold','--th',default=45)
+    parser.add_option('--personToWarn','-p',
+        default='tmackall@qualcomm.com')
+    parser.add_option('--fs','--fileSystems', dest='fs',
+        default='/media-mackall/av',
+        help='--fs \'/media-mackall/av,/home/tmackall\'')
+    parser.add_option('-l', '--logging-level', help='Logging Level',
+        default=logging.ERROR,dest='ll')
+
+    # process logging
     (options, args) = parser.parse_args()
+    loggingLevel = LOGGING_LEVELS.get(options.ll,logging.NOTSET)
+    logging.basicConfig(format='%(levelname)s - %(message)s',  \
+        level=loggingLevel)
+
 
     whereToEmail=options.personToWarn
     warningPercentage=int(options.threshold)
-    listFilers=['/home/media']
+    listFilers=options.fs.split(',')
+    # spin the file systems to check
     for i in listFilers:
+        logging.debug('File system %s' % i)
         cmd='uname -a'
         output=subprocess.check_output(cmd, shell=True)
         match=re.search(r'^Linux ([^\s]+) ',output)
-	machineName=match.group(1)
+        machineName=match.group(1)
         cmd1='cd ' + i
         cmd2='df .'
         cmd=cmd1 + ";" + cmd2
@@ -31,13 +52,18 @@ def Main():
             filer=i
             if percentInUse>warningPercentage:
                 subject="Server: " + machineName + " Filer: " + \
-			filer + " threshold warning!"
-                text="The current usage of the filer is: " \
-                + str(percentInUse)+'%'
-                emailMessage (whereToEmail, subject, text)
+                    filer + " threshold warning!"
+                text='The current usage of %s is: %i%% '\
+                    'and the threshold is %i%%'  \
+                    % (filer,percentInUse,warningPercentage)
+                logging.info(text)
+                if whereToEmail != "":
+                    emailMessage (whereToEmail, subject, text)
+                else:
+                    logging.warning('No email address given')
             else:
-                print str(percentInUse) + ' is  below the threshold of ' + \
-                str(warningPercentage)
+                logging.info ('%i%% is below the threshold of %i%% for %s' \
+                    % (percentInUse,warningPercentage,filer))
     exit(0)
 
 if __name__ == "__main__":
