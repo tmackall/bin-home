f92fc6498d51a0889016c7780404b8c1450d5d83
Tom Mackall <mackall.tom@gmail.com>
2012-07-25 05:14:32 -0600

    Updated to mackall-media

--- backupHome.sh
+++ backupHome.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-tar -zcvf /tmp/mackall-lnx2-`date +"%m-%d-%y"`.tgz -X /home/tmackall/bin/exclude.txt /home/tmackall
-echo "backup is locatedf at /tmp/mackall-lnx2-`date +"%m-%d-%y"`.tgz"
-scp /tmp/mackall-lnx2-`date +"%m-%d-%y"`.tgz "tmackall-mac.local:/Volumes/BACKUP\ #3/"
-#rm /tmp/mackall-lnx2-`date +"%m-%d-%y"`.tgz
+tar -zcvf /tmp/mackall-media-`date +"%m-%d-%y"`.tgz -X /home/tmackall/bin/exclude.txt /home/tmackall
+echo "backup is located at /tmp/mackall-media-`date +"%m-%d-%y"`.tgz"
+#scp /tmp/mackall-media-`date +"%m-%d-%y"`.tgz "tmackall-mac1.local:/Volumes/JULY-BCK/"
+#rm /tmp/mackall-media-`date +"%m-%d-%y"`.tgz
