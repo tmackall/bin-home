7838ce5e940625e892546a017f918da7cfe95786
Mackall <tmackall@mackall.(none)>
2012-09-21 06:26:01 -0600

    move startDjangoServer.sh to django/start_django_server.sh

---
+++ django/start_django_server.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+. $HOME/.bashrc
+nohup /home/tmackall/django_projects/mackallHouse/manage.py runserver mackall:8000 > /home/tmackall/django.log 2>&1 &
+exit 0
+
+
--- startDjangoServer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-. $HOME/.bashrc
-nohup /home/tmackall/django_projects/mackallHouse/manage.py runserver 192.168.1.23:8000 > /home/tmackall/django.log 2>&1 &
-exit 0
-
-
