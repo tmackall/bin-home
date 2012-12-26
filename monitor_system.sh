125cf2d427c8a08f843f9b9109a8f44f7e30d844
Tom Mackall <mackall.tom@gmail.com>
2012-12-26 06:01:25 -0700

    added more commands to monitor_system.sh

--- monitor_system.sh
+++ monitor_system.sh
@@ -2,11 +2,25 @@
 source ~/.bashrc
 
 TEMP_FILE=$(mktemp /tmp/XXXX)
-
-SUBJECT="Home computer system stats"
-cat /proc/cpuinfo > ${TEMP_FILE}
+host_name=$(hostname)
+SUBJECT="${host_name} computer system stats"
+echo -e "Uptime info: " >> ${TEMP_FILE}
+uptime >> ${TEMP_FILE}
+echo -e "\n\nFree info: " >> ${TEMP_FILE}
+free >> ${TEMP_FILE}
+echo -e "\n\niostat info: " >> ${TEMP_FILE}
+iostat >> ${TEMP_FILE}
+echo -e "\n\nDisk Usage Info: " >> ${TEMP_FILE}
 df -a >> ${TEMP_FILE}
-~/develop/email_msg.py -e tmackall@qualcomm.com -m ${TEMP_FILE} -s "${SUBJECT}"
+NETSTAT_FILE=$(mktemp /tmp/XXXX)
+echo -e "\n\nnetstat -a Info: " >> ${NETSTAT_FILE}
+netstat -a >> ${NETSTAT_FILE}
+echo -e "\n\npstree Info: " >> ${TEMP_FILE}
+pstree >> ${TEMP_FILE}
+
+#~/develop/email_msg.py -e tmackall@qualcomm.com -m ${TEMP_FILE} -s "${SUBJECT}"
+mutt tmackall@qualcomm.com -a /proc/meminfo -a /proc/cpuinfo -a ${NETSTAT_FILE}  -s "${SUBJECT}" < ${TEMP_FILE} 
 
 
+rm ${NETSTAT_FILE}
 rm ${TEMP_FILE}
