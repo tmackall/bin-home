95a25bae834fa33bf811d2287b679c8b971d9f21
Tom Mackall <mackall.tom@gmail.com>
2013-02-22 04:58:01 -0700

    pointed to bin dir for monitor_disks.sh

--- monitor_disks.sh
+++ monitor_disks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source /home/tmackall/.bashrc
 #for i in / /disk1 /disk2 /disk3; do 
-~/develop/get_disk_space.py --fs / --th 12
-~/develop/get_disk_space.py --fs /disk1 --th 65
-~/develop/get_disk_space.py --fs /disk2 --th 10
-~/develop/get_disk_space.py --fs /disk3 --th 98
+~/bin/get_disk_space.py --fs / --th 12
+~/bin/get_disk_space.py --fs /disk1 --th 65
+~/bin/get_disk_space.py --fs /disk2 --th 10
+~/bin/get_disk_space.py --fs /disk3 --th 98
