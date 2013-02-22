9cf5b399a254cd825db69b49459e350372bd00e6
Tom Mackall <mackall.tom@gmail.com>
2013-02-22 04:56:25 -0700

    upped disk1 from 55% to 65% in monitor_disks.sh

--- monitor_disks.sh
+++ monitor_disks.sh
@@ -2,6 +2,6 @@
 source /home/tmackall/.bashrc
 #for i in / /disk1 /disk2 /disk3; do 
 ~/develop/get_disk_space.py --fs / --th 12
-~/develop/get_disk_space.py --fs /disk1 --th 55
+~/develop/get_disk_space.py --fs /disk1 --th 65
 ~/develop/get_disk_space.py --fs /disk2 --th 10
 ~/develop/get_disk_space.py --fs /disk3 --th 98
