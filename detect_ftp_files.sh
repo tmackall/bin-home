8ac850f1d99a8f024f0e89310e14e8ee40d92112
Tom Mackall <mackall.tom@gmail.com>
2013-03-30 06:59:15 -0600

    detect_ftp_files.sh - added camera information and logic to stop sending zip files when multiple detections per hour.

--- detect_ftp_files.sh
+++ detect_ftp_files.sh
@@ -5,11 +5,23 @@ zip_file=/tmp/detected_files.zip
 prev_files=/tmp/ftp_file_list.txt
 email1=mackall.tom@gmail.com
 email_text_file=/tmp/email_text_file.txt
+email_msg_only=3032411300@txt.att.net
+email_msg_body=/tmp/temp_email_body.txt
 ftp_dir=~/ftp
 tar_file="$ftp_dir/detected_files.$$.tgz"
+max_per_period=3
+period=60
+
+rm $zip_file
+
+# see how many tgz files have been created in the last hour. Quit
+# sending if more than 3
+#echo $(find "$ftp_dir" -mmin -${period}  -name "*.tgz")
+tgz_cnt=$(find "$ftp_dir" -mmin -${period}  -name "*.tgz" | wc | \
+    sed 's/\([0-9]\+\) .*/\1/' )
+#echo $tgz_cnt
 
 # any jpg files here mean they are new since we tar them up
-#file_list=$(find "${ftp_dir}" -cnewer $zip_file -name "*.jpg" )
 file_list=$(find "${ftp_dir}" -name "*.jpg" )
 status=$?
 if [[ $status -ne 0 ]]; then
@@ -19,12 +31,33 @@ if [[ $status -ne 0 ]]; then
 fi
 
 
+f_kitchen=0
+f_basement=0
 #
 # zip files to put in an email
-echo $file_list
 for file in ${file_list}; do
     zip -g $zip_file ${file}
+    # check to see what camera detected motion
+    if [[ $file =~ kitchen ]]; then
+        f_kitchen=1
+    elif [[ $file =~ basement ]]; then
+        f_basement=1
+    fi
 done
+
+
+#
+# message subject line
+echo "Camera count: $(($f_kitchen + $f_basement))"
+motion_subject=""
+if [[ $f_kitchen -eq 1 ]] && [[ $f_basement -eq 1 ]]; then
+    motion_subject="Motion was detected in the kitchen and basement"
+elif [[ $f_kitchen -eq 1 ]]; then
+    motion_subject="Motion was detected in the kitchen"
+else
+    motion_subject="Motion was detected in the basement"
+fi
+
 
 # tar up the files in the ftp dir to reduce space and clutter.
 cd ${ftp_dir}
@@ -37,14 +70,25 @@ echo "tar: $status"
 # are no jpegs
 if [[ $status -eq 0 ]]; then
     rm ${ftp_dir}/*.jpg
-    pwd > $email_text_file
-    ls -l >> $email_text_file
+
+    # get some ftp dir info
+    echo -e "$(pwd)\n" > $email_text_file
+    echo -e "$(ls -l)\n" >> $email_text_file
     df -a . >> $email_text_file
-    mutt mackall.tom@gmail.com -s "Motion detection alert" \
-        -a $zip_file < $email_text_file
-    rm $zip_file
+    
+    # for txt email only
+    echo -e $motion_subject > $email_msg_body
+    mutt $email_msg_only -s "Motion detection alert!"  < $email_msg_body
+    # send the zipped jpg files
+    if [[ $tgz_cnt -gt 2 ]]; then
+        echo -e "\nHalted sending pics due to freqency: $tgz_cnt per $period mins" >> $email_text_file
+        mutt $email1 -s "$motion_subject"  < $email_text_file
+    else
+        mutt $email1 -s "$motion_subject" -a $zip_file < $email_text_file
+    fi
     echo $?
 else
+    # will create an empty tar file that I don't want
     rm ${tar_file}
 fi
 
