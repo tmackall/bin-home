0ef9dce993653e9fef0ae24902f7cbc36b407da3
Tom Mackall <mackall.tom@gmail.com>
2013-04-07 08:10:49 -0600

    monitor_disks.sh - update to be a bash solution

--- monitor_disks.sh
+++ monitor_disks.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
-source /home/tmackall/.bashrc
-#for i in / /disk1 /disk2 /disk3; do 
-~/bin/get_disk_space.py --fs / --th 12
-~/bin/get_disk_space.py --fs /disk1 --th 65
-~/bin/get_disk_space.py --fs /disk2 --th 10
-~/bin/get_disk_space.py --fs /disk3 --th 98
+
+script=$0
+email_addrs=mackall.tom@gmail.com
+
+# these are the filesystem we are concerned with
+
+# key-value pairs for filesystems
+declare -A fsArr
+fsArr["/"]=12
+fsArr["/disk1"]=65
+fsArr["/disk2"]=10
+fsArr["/disk3"]=98
+# spin the file system array
+for fs in ${!fsArr[@]}; do
+    th=${fsArr[${fs}]}
+    output=$(get_disk_space_pcent.sh $fs)
+    status=$?
+    if [[ $status -ne 0 ]]; then
+        subject="$script failed with error code: $status"
+        echo -e $output | mutt -s "$subject" $email_addrs
+        continue 
+    fi
+    if [[ $output -gt $th ]]; then
+        host=$(hostname)
+        subject="Filesystem \"$fs\" threshold exceeded ($th%) for host:$host"
+        echo -e $output | mutt -s "$subject" $email_addrs
+    fi
+done
+
+exit 0
