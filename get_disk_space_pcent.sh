c55c131c0372b136880d6c7381634142a738b7ba
Tom Mackall <mackall.tom@gmail.com>
2013-04-07 08:11:57 -0600

    get_disk_space_pcent.sh - bash solution for getting disk space

---
+++ get_disk_space_pcent.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+source ~tmackall/.bashrc
+
+script=$0
+fs="$1"
+
+if [[ $fs =~ ^\ *$ ]]; then
+    echo -e "\n$script failed: $script [file system]\n"
+    exit 2
+fi
+
+
+output=$(df $1)
+status=$?
+if [[ $status -ne 0 ]]; then
+    echo -e "\nFile System: $1 is likely invalid\n"
+    exit 3
+fi
+
+pcent=$(echo $output | sed 's/.* \([0-9]\+\)% .*/\1/') 
+echo $pcent
+exit 0
