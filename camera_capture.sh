2245b083f21ae486543c41ed4b58feaafc52c3ec
Tom Mackall <mackall.tom@gmail.com>
2013-11-09 14:01:53 -0700

    camera_capture.sh - made runtime 3800 sec - 3 minutes over an hour so there is overlapw

--- camera_capture.sh
+++ camera_capture.sh
@@ -4,7 +4,7 @@ date_name=$(date +"%Y-%m-%d-%H:%M:%S")
 ip_sno01="192.168.1.15"
 
 name="${date_name}_sno01.mp4"
-output=$(cvlc --run-time 30 rtsp://${ip_sno01}/H.264/media.smp --rtsp-user=admin --rtsp-pwd=dampob12 --sout="#duplicate{dst=standard{access=file,dst='/home/tmackall/video/${name}',mux=mp4}" vlc://quit)
+output=$(cvlc --run-time 3800 rtsp://${ip_sno01}/H.264/media.smp --rtsp-user=admin --rtsp-pwd=dampob12 --sout="#duplicate{dst=standard{access=file,dst='/home/tmackall/video/${name}',mux=mp4}" vlc://quit)
 
 
 status=$?
