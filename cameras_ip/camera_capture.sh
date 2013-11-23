2e17c67fbad3d0634a85bbba9e402c4464d1f06f
Tom Mackall <mackall.tom@gmail.com>
2013-11-23 08:19:43 -0700

    camera_capture.sh - added the correct path to the XML file

--- cameras_ip/camera_capture.sh
+++ cameras_ip/camera_capture.sh
@@ -9,9 +9,10 @@ fi
 
 cam_id=$1
 time_secs=$2
+CAMERA_XML="/home/tmackall/bin/cameras_ip/cameras.xml"
 #
 # grab the unique camera IDs from the XML
-vals=$(xmlstarlet sel -t -m "//camera" -v "@id" -o " " cameras.xml)
+vals=$(xmlstarlet sel -t -m "//camera" -v "@id" -o " " ${CAMERA_XML})
 match=0
 #
 # loop - all unique IDs
@@ -27,13 +28,13 @@ if [[ $match -eq 0 ]]; then
     exit 3
 fi
 cam_ip=$(xmlstarlet sel -t -m "//camera[@id='$cam_id']" \
-    -v "ip_address" cameras.xml)
+    -v "ip_address" ${CAMERA_XML})
 cam_url=$(xmlstarlet sel -t -m "//camera[@id='$cam_id']" \
-    -v "rtsp_loc" cameras.xml)
+    -v "rtsp_loc" ${CAMERA_XML})
 cam_user=$(xmlstarlet sel -t -m "//camera[@id='$cam_id']" \
-    -v "user" cameras.xml)
+    -v "user" ${CAMERA_XML})
 cam_pass=$(xmlstarlet sel -t -m "//camera[@id='$cam_id']" \
-    -v "passwd" cameras.xml)
+    -v "passwd" ${CAMERA_XML})
 rstp_cmd="rtsp://${cam_ip}/${cam_url} --rtsp-user=${cam_user} --rtsp-pwd=${cam_pass}"
 name="${date_name}_${cam_id}.mp4"
 cmd="cvlc --run-time ${time_secs}  $rstp_cmd --sout=\"#duplicate{dst=standard{access=file,dst='/disk2/camera_video_backups/${name}',mux=mp4}\" vlc://quit"
