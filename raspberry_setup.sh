a8afbe9a5eb7aeaef549a07aa8347ffac6951a94
Tom Mackall <mackall.tom@gmail.com>
2013-12-28 13:24:10 +0000

    raspberry_setup.sh - added notes for additional packages and git repo installs.

--- raspberry_setup.sh
+++ raspberry_setup.sh
@@ -6,3 +6,14 @@ ssh-keygen -t dsa
 sudo apt-get install vim
 ssh-keygen -t dsa
 scp ~/.ssh/id_dsa.pub <xxxxx>:.ssh/authorized_keys2
+sudo apt-get install git
+cd /home
+sudo mv tmackall tmackall.bck
+sudo chmod 777 .
+git clone ssh://mackall-home/disk1/github-mackall/homedir.git tmackall
+sudo chmod 775 tmackall
+cd tmackall
+git clone ssh://mackall-home/disk1/github-mackall/bin.git bin
+sudo apt-get install vim python-dev python-setuptools nginx supervisor
+sudo apt-get install python-pip
+sudo pip install virtualenv virtualenvwrapper
