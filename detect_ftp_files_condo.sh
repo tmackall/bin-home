de3c9ce0331e41495e35f9370605016096048e25
Tom Mackall <mackall.tom@gmail.com>
2013-12-29 05:50:21 -0700

    updated with a copy to the home server.

--- detect_ftp_files_condo.sh
+++ detect_ftp_files_condo.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 source ~/.bashrc
 
-zip_file=/tmp/detected_files.zip
-prev_files=/tmp/ftp_file_list.txt
-email1="mackall.tom@gmail.com"
+email_list="mackall.tom@gmail.com"
 email_text_file=/tmp/email_text_file.txt
-email_msg_only=3032411300@txt.att.net
 email_msg_body=/tmp/temp_email_body.txt
 ftp_dir=/home/tmackall/ftp
 epoch_secs=$(date +%d-%b-%Y-%s)
+zip_file="/tmp/detected_files_${epoch_secs}.zip"
+disk_home_server="/disk1/mackall-rp03/ftp"
 
 # any jpg files here mean they are new since we tar them up
 file_list=$(find $ftp_dir -name "*.jpg")
@@ -17,7 +16,7 @@ status=$?
 if [[ $status -ne 0 ]]; then
     fail_text="find command failed: $status"
     echo $fail_text
-    mutt $email1 -s "$fail_text" < /dev/null
+    mutt $email_list -s "$fail_text" < /dev/null
 fi
 
 # bail if there are no new .jpg's
@@ -26,19 +25,15 @@ if [[ $file_list == "" ]]; then
     exit 0
 fi
 
-#
+
 # grab a single jpeg in the middle to send as a sample
-file_cnt=$(echo "$file_list" | wc |\
-    sed 's/[0-9]\+ \+\([0-9]\+\) .*/\1/')
+file_cnt=$(echo "$file_list" | wc | sed 's/[0-9]\+ \+\([0-9]\+\) .*/\1/')
 a_files=($file_list)
 middle=$(($file_cnt / 2))
 pics="${a_files[$middle]}"
 echo "$pics"
 motion_string=""
-temp_file="/tmp/detect_ftp_files.tmp"
-rm $temp_file
-touch $temp_file
-#
+
 # zip files to put in an email
 cd "${ftp_dir}"
 for file_and_path in ${file_list}; do
@@ -48,38 +43,38 @@ for file_and_path in ${file_list}; do
 
     # zip the files
     #zip -g $zip_file ${file_and_path}
-    zip -g $zip_file ${file}
+    zip -g $zip_file ${file} > /dev/null
 
 done
 
-#motion_string=$(cat $temp_file | sort | uniq)
+# email subject
 motion_string="Condo-Cam"
-#
+
 # message subject line
 motion_subject="\"Motion was detected on the following cameras: $motion_string\""
-echo $motion_subject
 
 
 # get some ftp dir info
 echo -e "Number of image files: $file_cnt\n" > $email_text_file
-#echo -e "$(pwd)\n" >> $email_text_file
-#echo -e "$(ls -lrt | \
-#    sed 's/.* \+\(... \+[0-9]\+ \+[0-9]\{2\}:.*\)/\1/')\n" >> \
-#    $email_text_file
 disk_use=$(df -a . | grep -Po "\d+%" )
-echo -e "Use: $disk_use\n" >> $email_text_file
 
 # for txt email only
 echo -e $motion_subject > $email_msg_body
 echo -e "disk use: $disk_use, Num imgs: $file_cnt" >> $email_msg_body
-# send the zipped jpg files
-cmd="mutt $email1 -s "${motion_subject}" -a $pics -a $zip_file \
-    < $email_text_file"
+echo -e "location: $disk_home_server, file:$zip_file" >> $email_msg_body
+
+# email notification
+cmd="mutt $email_list -s "${motion_subject}" -a $pics < $email_msg_body"
 output=$(eval $cmd)
 status=$?
+
+
+# ZIP file - move to home server
+cmd="scp $zip_file mackall-home:/${disk_home_server}/"
+output=$(eval $cmd)
+
+# cleanup
 rm $file_list
 rm $zip_file
 
 exit $status
-
-
