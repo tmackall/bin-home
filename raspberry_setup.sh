18171070e957c321fd31faa336afc0fbef8b6835
Tom Mackall <mackall.tom@gmail.com>
2014-01-01 08:56:51 -0700

    raspberry_setup.sh - more updates

--- raspberry_setup.sh
+++ raspberry_setup.sh
@@ -3,22 +3,25 @@
 sudo adduser tmackall
 sudo adduser tmackall sudo
 # raspberry config
+# set timezone, disk, over-clock etc
 sudo raspi-config
 # get current os updates
 sudo apt-get update
 sudo apt-get upgrade
-# timezone
-sudo dpkg-reconfigure tzdata
-ssh-keygen -t rsa
 sudo apt-get install vim
 scp ~/.ssh/id_dsa.pub <xxxxx>:.ssh/authorized_keys2
 sudo apt-get install git
 cd /home
 sudo mv tmackall tmackall.bck
+# add mackall-home etc to this.
+sudo vim /etc/hosts 
 sudo chmod 777 .
 git clone ssh://mackall-home/disk1/github-mackall/homedir.git tmackall
 sudo chmod 775 tmackall
+sudo rm -rf tmackall.bck
+sudo reboot
 cd tmackall
+ssh-keygen -t rsa
 git clone ssh://mackall-home/disk1/github-mackall/bin.git bin
 sudo apt-get install vim python-dev python-setuptools nginx supervisor
 sudo apt-get install python-pip
@@ -29,3 +32,4 @@ sudo apt-get install dnsutils
 sudo apt-get install vsftpd
 sudo apt-get install sysstat
 sudo apt-get install zip
+sudo apt-get install vlc
