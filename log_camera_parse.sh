582d12452ddcf627d1ce4e5b0368e7216a60dded
Tom Mackall <mackall.tom@gmail.com>
2014-02-08 09:52:45 -0700

    updated with epoch calc. Also store access time of video file.

--- log_camera_parse.sh
+++ log_camera_parse.sh
@@ -1,36 +1,61 @@
 #!/bin/bash
 
+#
+# init
+#------------------------------------------------------------
 dir_log=/disk2/camera_log_motion
 data=$(cat ${dir_log}/log.txt | sed 's/ *//g')
 dir_video=/disk2/camera_video_backups
 dir_copy=/disk3/video_cam_mvmt
 
 #
-# put the time into the name
-file_name_adj_list=""
+# video files 
+#------------------------------------------------------------
+file_videos_tmp="/tmp/file_videos_tmp.$$.txt"
+echo "" > $file_videos_tmp
 pushd "$dir_video"
-for i in $(ls  *.mp4); do
-    time=$(ls -l $i | awk '{print $8;}')
-    file_new=$(echo $i |\
-        sed  "s/-[0-9]\+:[0-9]\+:/-$time:/")
-    file_name_adj_list+="$file_new  "
+for i in $(ls); do
+    stuff=$(ls -l --time-style=full-iso $i | awk '{print $6,$7,$9}')
+    time_fmt=$(echo $stuff | awk '{print $1,$2}')
+    file_time=$(echo $stuff | awk '{print $3}')
+    camera=$(echo $i | sed s/.*_// | sed 's/\..*//')
+    time=$(echo $i | sed s/_.*//)
+    time_epoch=$(date -d "$time_fmt" +%s)
+    echo "$camera:$time_epoch:$time:v" >> $file_videos_tmp
 done
-popd
-for i in $file_name_adj_list; do
-    echo $i
-done
-exit
-for i in $data; do
-    file_search=$(echo $i | sed 's/\(.*\),\(.*\):..$/\2*_\1.mp4/')
-    file_name=$(find $dir_video -name $file_search)
-    echo "file name:$file_name"
-    if [[ ! file_name == "" ]]; then
-        cmd="mv $file_name $dir_copy"
-        echo $cmd
-        #eval "$cmd"
-    fi
-done
-tail -n 6 "${dir_log}/log.txt" > /tmp/log.txt
-mv /tmp/log.txt "${dir_log}/log.txt"
-exit 0
+popd "$dir_video"
 
+#
+# data triggers from log
+#------------------------------------------------------------
+file_data_tmp="/tmp/file_data_tmp.$$.txt"
+echo "" > $file_data_tmp
+for i in $data; do
+    camera=$(echo $i | sed s/,.*//)
+    time=$(echo $i | sed s/.*,//)
+    time_fmt=$(echo $time | sed 's/-\(..:.*\)$/ \1/') 
+    time_epoch=$(date -d "$time_fmt" +%s)
+    echo "$camera:$time_epoch:$time:l" >> $file_data_tmp
+done
+
+
+#
+# data - move around and sort it.
+#------------------------------------------------------------
+file_combined="/tmp/file_combined.$$.txt"
+file_combined_sorted="/tmp/file_combined_sorted.$$.txt"
+cat $file_videos_tmp > $file_combined
+cat $file_data_tmp >> $file_combined
+cat $file_combined | sort > $file_combined_sorted
+echo $file_combined_sorted
+
+#
+# cleanup
+#------------------------------------------------------------
+rm $file_videos_tmp
+rm $file_data_tmp
+rm $file_combined
+#tail -n 6 "${dir_log}/log.txt" > /tmp/log.txt
+#mv /tmp/log.txt "${dir_log}/log.txt"
+exit 0
+#
