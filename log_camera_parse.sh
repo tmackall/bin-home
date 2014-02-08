e9b5025225c54e8323bb7dbc50b5440d5ad6c4e3
Tom Mackall <mackall.tom@gmail.com>
2014-02-08 06:56:11 -0700

    save a version \(that doesn't work\) just in case I want to get the code back

--- log_camera_parse.sh
+++ log_camera_parse.sh
@@ -4,6 +4,22 @@ dir_log=/disk2/camera_log_motion
 data=$(cat ${dir_log}/log.txt | sed 's/ *//g')
 dir_video=/disk2/camera_video_backups
 dir_copy=/disk3/video_cam_mvmt
+
+#
+# put the time into the name
+file_name_adj_list=""
+pushd "$dir_video"
+for i in $(ls  *.mp4); do
+    time=$(ls -l $i | awk '{print $8;}')
+    file_new=$(echo $i |\
+        sed  "s/-[0-9]\+:[0-9]\+:/-$time:/")
+    file_name_adj_list+="$file_new  "
+done
+popd
+for i in $file_name_adj_list; do
+    echo $i
+done
+exit
 for i in $data; do
     file_search=$(echo $i | sed 's/\(.*\),\(.*\):..$/\2*_\1.mp4/')
     file_name=$(find $dir_video -name $file_search)
@@ -11,7 +27,7 @@ for i in $data; do
     if [[ ! file_name == "" ]]; then
         cmd="mv $file_name $dir_copy"
         echo $cmd
-        eval "$cmd"
+        #eval "$cmd"
     fi
 done
 tail -n 6 "${dir_log}/log.txt" > /tmp/log.txt
