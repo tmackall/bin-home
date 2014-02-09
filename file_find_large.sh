313c01348bca09ce51901fcffaf074a01980385f
Tom Mackall <mackall.tom@gmail.com>
2014-02-09 15:50:08 -0700

    file_find_large.sh - now has a file size threshold.

--- file_find_large.sh
+++ file_find_large.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-if [[ $# < 1 ]]; then
-    echo "Usage: $0 <dir head>"
+if [[ $# < 2 ]]; then
+    echo "Usage: $0 <dir head> <size kilobytes>"
     exit 1
 fi
 
 dir_head=$1
+size=$2
 # find 10M+ files
-find ${dir_head} -type f -size +10000k -exec ls -lh {} \; | awk '{ print $9 ": " $5 }'
+find ${dir_head} -type f \( ! -name mnt \) -size +${size}k -exec ls -lh {} \; | awk '{ print $9 ": " $5 }'
