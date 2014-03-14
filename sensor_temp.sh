74a1af3cbfc3e8b5d1a60317991eb02ccba8f047
Tom Mackall <mackall.tom@gmail.com>
2014-03-14 11:56:19 -0600

    sensor_temp.sh - made it only send email when outside of a range,

--- sensor_temp.sh
+++ sensor_temp.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 
-pushd /home/tmackall/bin
-TEMP_STRING=$(sudo ./sensor_temp.py)
-popd
+TEMP_STRING=$(curl --data "test=false" http://mackall-rp04:8000/sensor_temp/)
 
-TEMP=$(echo "$TEMP_STRING" | sed 's/.*) *//')
-SUBJECT="House temp is: $TEMP"
-echo "$SUBJECT"
-mutt "mackall.tom@gmail.com" -s "${SUBJECT}" < /dev/null
+TEMP_FMT=$(echo "$TEMP_STRING" | sed 's/.*) *//')
+TEMP=$(echo "$TEMP_FMT" | sed 's/ *F//')
+RESULT1=$(echo "$TEMP > 71.0" | bc)
+RESULT2=$(echo "$TEMP < 65.0" | bc)
+echo "$RESULT1"
+if [[ $RESULT1 -ne 0 ]] || [[ $RESULT2 -ne 0 ]]; then
+    SUBJECT="Warning: house temp is: $TEMP"
+    echo "$SUBJECT"
+    mutt "mackall.tom@gmail.com" -s "${SUBJECT}" < /dev/null
+fi
