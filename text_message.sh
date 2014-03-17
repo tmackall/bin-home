73533e8f01cbdddd584d913c7c2de292560b22fe
Tom Mackall <mackall.tom@gmail.com>
2014-03-17 09:24:24 -0600

    text_message.sh - initial revision

---
+++ text_message.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+usage()
+{
+cat << EOF
+usage: $0 options
+
+This script run the test1 or test2 over a machine.
+
+OPTIONS:
+   -h      help
+   -m      message
+   -n      numnber (3032411300)
+EOF
+}
+
+MESSAGE=""
+NUMBER=""
+while getopts “hm:n:” OPTION
+do
+	case $OPTION in
+	h)
+	    usage
+	    exit 4
+	    ;;
+	m)
+	    MESSAGE=$OPTARG
+	    ;;
+	n)
+	    NUMBER=$OPTARG
+	    ;;
+	?)
+	    usage
+	    exit
+	    ;;
+	esac
+done
+
+if [[ "$MESSAGE" == "" ]] || [[ "$NUMBER" == "" ]]; then
+    usage
+    exit 1
+fi
+curl http://textbelt.com/text -d number=$NUMBER -d "message=$MESSAGE"
+
+
