bd4aeb9699c9952f76dd07879687b19704e9a1c6
Tom Mackall <mackall.tom@gmail.com>
2014-03-17 09:49:45 -0600

    sensor_temp.sh - added text messaging on out of range tempatures.

--- sensor_temp.sh
+++ sensor_temp.sh
@@ -7,9 +7,10 @@ TEMP_FMT=$(echo "$TEMP_STRING" | sed 's/.*) *//')
 TEMP=$(echo "$TEMP_FMT" | sed 's/ *F//')
 RESULT1=$(echo "$TEMP > 71.0" | bc)
 RESULT2=$(echo "$TEMP < 65.0" | bc)
-echo "$RESULT1"
+echo -e "\n${TEMP}F\n"
 if [[ $RESULT1 -ne 0 ]] || [[ $RESULT2 -ne 0 ]]; then
     SUBJECT="Warning: house temp is: $TEMP"
     echo "$SUBJECT"
     mutt "mackall.tom@gmail.com" -s "${SUBJECT}" < /dev/null
+    text_message.sh -n 3032411300 -m "$SUBJECT"
 fi
