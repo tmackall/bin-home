65e410da6583b4858c7752556b9520c5fd6309ed
Tom Mackall <mackall.tom@gmail.com>
2014-05-21 06:25:51 -0600

    ip_external_get.sh

--- ip_external_get.sh
+++ ip_external_get.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 ip_file="/tmp/ip_external.txt"
-#ip_now=$(curl -s icanhazip.com )
-ip_now=$(dig +short myip.opendns.com @resolver1.opendns.com)
+ip_now=$(curl -s icanhazip.com )
+#ip_now=$(dig +short myip.opendns.com @resolver1.opendns.com)
 echo $ip_now
+if [[ ! "$ip_now" =~ \d+\.\d+\.\d+\.\d+ ]]; then
+    email_subject="IP get failure: $ip_now"
+    mutt mackall.tom@gmail.com -s "${email_subject}" < /dev/null
+    exit 1
+fi
 ip_external=$(cat $ip_file)
 if [[ "$ip_now" != "$ip_external" ]]; then
     email_subject="IP:${ip_external} has changed to IP:${ip_now}"
