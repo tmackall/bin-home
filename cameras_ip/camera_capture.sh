d3d3f4b9621230d9548d9bc211b4cdbe0447ec76
Tom Mackall <mackall.tom@gmail.com>
2016-03-29 07:13:20 -0600

    branched the RPI camera code.

--- cameras_ip/camera_capture.sh
+++ cameras_ip/camera_capture.sh
@@ -8,18 +8,28 @@
 date_name=$(date +"%Y-%m-%d-%H:%M:%S")
 time_secs=$((60 * 60))
 
-if [[ $# -ne 2 ]]; then
-    echo "Usage: $0 <camera id> <time (in secs)>\n"
+if [[ $# -lt 2 ]]; then
+    echo "Usage: $0 <camera id> <time (in secs)> [root dir]\n"
     exit 2
 fi
 
 cam_id=$1
 time_secs=$2
+#
+# root dir - optional
+if [[ $3 == "" ]]; then
+    ROOT="/disk2"
+else
+    ROOT=$3
+fi
+
 CAMERA_XML="/home/tmackall/bin/cameras_ip/cameras.xml"
+
 #
 # grab the unique camera IDs from the XML
 vals=$(xmlstarlet sel -t -m "//camera" -v "@id" -o " " ${CAMERA_XML})
 match=0
+
 #
 # loop - all unique IDs
 for i in $vals
@@ -46,7 +56,7 @@ cam_pass=$(xmlstarlet sel -t -m "//camera[@id='$cam_id']" \
     -v "passwd" ${CAMERA_XML})
 rstp_cmd="rtsp://${cam_ip}/${cam_url} --rtsp-user=${cam_user} --rtsp-pwd=${cam_pass}"
 name="${date_name}_${cam_id}.mp4"
-cmd="cvlc --run-time ${time_secs} --rtsp-tcp  $rstp_cmd --sout=\"#duplicate{dst=standard{access=file,dst='/disk2/camera_video_backups/${name}',mux=mp4}\" vlc://quit"
+cmd="cvlc --run-time ${time_secs} --rtsp-tcp  $rstp_cmd --sout=\"#duplicate{dst=standard{access=file,dst='${ROOT}/camera_video_backups/${name}',mux=mp4}\" vlc://quit"
 echo "$cmd"
 eval $cmd
 status=$?
